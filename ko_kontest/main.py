935e4a4e2214143ecb6a6ee38aa9ccac2f76caf3
Jakub Jíra <jakub.jira@rixo.cz>
2022-04-25 14:51:38 +0200

    0 and 1 correct, else incorrect

--- ko_kontest/main.py
+++ ko_kontest/main.py
@@ -9,61 +9,7 @@ import queue
 
 def my_callback(model, where):
     if where == g.GRB.Callback.MIPSOL:
-        # my_terminal = -1
-        # visited = [False for i in range(number_of_nodes)]
-        # for terminal in range(number_of_nodes):
-        #     parent = [-1 for i in range(number_of_nodes)]
-        #     if nodes[terminal] == 1 and not visited[terminal]:
-        #         visited[terminal] = True
-        #         my_terminal = terminal
-        #         my_set = {my_terminal}
-        #         local_queue = queue.Queue()
-        #         q = queue.Queue()
-        #         local_queue.put(my_terminal)
-        #         while not local_queue.empty():
-        #             current_node = local_queue.get()
-        #             for i in range(number_of_nodes):
-        #                 if model.cbGetSolution(used_edges[current_node, i]) == 1:
-        #                     local_queue.put(i)
-        #                     parent[i] = current_node
-        #                     my_set.add(i)
-        #                 elif model.cbGetSolution(used_edges[i, current_node]) == 1:
-        #                     local_queue.put(i)
-        #                     parent[i] = current_node
-        #                     my_set.add(i)
-        #                 elif distance_matrix[current_node][i] is not sys.maxsize:
-        #                     parent[i] = current_node
-        #                     q.put(i)
-        #         terminal_count = g.quicksum(nodes[i] for i in my_set)
-        #         if terminal_count is not number_of_terminals:
-        #             while not q.empty():
-        #                 solution_found = False
-        #                 current_node = q.get()
-        #                 for i in range(number_of_nodes):
-        #                     if distance_matrix[current_node][i] == sys.maxsize:
-        #                         continue
-        #                     if not visited[i]:
-        #                         if nodes[i] == 1:
-        #                             # add path to constrains and beak to outer cycle
-        #                             parent[i] = current_node
-        #                             current_parent = parent[i]
-        #                             while nodes[current_parent] != 1:
-        #                                 model.cbLazy(used_edges[current_parent, i] == 1)
-        #                                 i = current_parent
-        #                                 current_parent = parent[i]
-        #                             model.cbLazy(used_edges[current_parent, i] == 1)
-        #                             solution_found = True
-        #                             break
-        #                         else:
-        #                             parent[i] = current_node
-        #                             q.put(i)
-        #                 if solution_found:
-        #                     break
-        #         else:
-        #             return
-        # if my_terminal == -1:
-        #     print("some error happened")
-        #     return
+        x = model.cbGetSolution(used_edges)
         my_terminal = -1
         visited = [False for i in range(number_of_nodes)]
         for terminal in range(number_of_nodes):
@@ -73,32 +19,59 @@ def my_callback(model, where):
                 my_terminal = terminal
                 my_set = {my_terminal}
                 local_queue = queue.Queue()
-                # q = queue.Queue()
+                q = queue.PriorityQueue()
                 local_queue.put(my_terminal)
                 while not local_queue.empty():
                     current_node = local_queue.get()
                     for i in range(number_of_nodes):
-                        if model.cbGetSolution(used_edges[current_node, i]) == 1:
+                        if visited[i]:
+                            continue
+                        if x[current_node, i] == 1:
+                            visited[i] = True
                             local_queue.put(i)
                             parent[i] = current_node
                             my_set.add(i)
-                        elif model.cbGetSolution(used_edges[i, current_node]) == 1:
+                        elif x[i, current_node] == 1:
+                            visited[i] = True
                             local_queue.put(i)
                             parent[i] = current_node
                             my_set.add(i)
                         elif distance_matrix[current_node][i] is not sys.maxsize:
                             parent[i] = current_node
-                            # q.put(i)
-                terminal_count = sum(nodes[i] for i in my_set)
-                if terminal_count < number_of_terminals:
-                    out_edges = 0
-                    for i in my_set:
-                        for j in range(number_of_nodes):
-                            out_edges += model.cbGetSolution(used_edges[i, j]) + model.cbGetSolution(used_edges[j, i])
-                    model.cbLazy(g.quicksum(used_edges[i, j] for i in my_set for j in range(number_of_nodes)) +
-                                 g.quicksum(used_edges[j, i] for i in my_set for j in range(number_of_nodes)) >= out_edges + 1)
+                            q.put((distance_matrix[current_node][i], i))
+                terminal_count = g.quicksum(nodes[i] for i in my_set)
+                if terminal_count is not number_of_terminals:
+                    while not q.empty():
+                        solution_found = False
+                        cost, current_node = q.get()
+                        visited[current_node] = True
+                        if nodes[current_node] == 1:
+                            model.cbLazy(used_edges[current_node, parent[current_node]] == 1)
+                            solution_found = True
+                            break
+                        for i in range(number_of_nodes):
+                            if distance_matrix[current_node][i] == sys.maxsize:
+                                continue
+                            if not visited[i]:
+                                if nodes[i] == 1:
+                                    # add path to constrains and beak to outer cycle
+                                    parent[i] = current_node
+                                    current_parent = parent[i]
+                                    while nodes[current_parent] != 1:
+                                        model.cbLazy(used_edges[current_parent, i] == 1)
+                                        i = current_parent
+                                        current_parent = parent[i]
+                                    model.cbLazy(used_edges[current_parent, i] == 1)
+                                    solution_found = True
+                                    break
+                                else:
+                                    parent[i] = current_node
+                                    q.put((distance_matrix[current_node][i]+cost, i))
+                        if solution_found:
+                            break
                 else:
                     return
+            # break
         if my_terminal == -1:
             print("some error happened")
             return
@@ -128,7 +101,7 @@ if __name__ == '__main__':
     print(nodes)
 
     m = g.Model()
-    # m.Params.lazyConstraints = 1
+    m.Params.lazyConstraints = 1
     used_nodes = m.addVars(number_of_nodes, vtype=g.GRB.BINARY)
     used_edges = m.addVars(number_of_nodes, number_of_nodes, vtype=g.GRB.BINARY)
 
@@ -144,7 +117,7 @@ if __name__ == '__main__':
     # minimize cost of spanning tree
     m.setObjective(g.quicksum(used_edges[i, j] * distance_matrix[i][j] for i in range(number_of_nodes) for j in range (number_of_nodes)), g.GRB.MINIMIZE)
 
-    m.optimize()
+    m.optimize(my_callback)
 
     print(str(m.objVal))
 
